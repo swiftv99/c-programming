1f500aca58c11889459e20678cbcad78dbe75885
Abdulaziz Musa <78258532+swiftv99@users.noreply.github.com>
2024-08-29 09:51:37 +0200

    Create char.c

---
+++ dataTypes/char.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    /*
+    Char Data Type
+    */
+    // Example 1
+    char myGrade = 'A';
+    printf("%c\n", myGrade);
+    
+    // Example 2
+    char a = 65, b = 66, c = 67;
+    printf("%c\n", a);
+    printf("%c\n", b);
+    printf("%c\n", c);
+    
+    // Example 3
+    char myText = 'Hello';
+    printf("%c\n", myText);
+
+    // Example 4
+    char myText[] = "Hello";
+    printf("%s\n", myText);
+    return 0;
+}
